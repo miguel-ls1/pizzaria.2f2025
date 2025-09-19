3ad085bae56aac915980ab4f54fafb02fd1bf1d5
miguel-ls1 <rm95373@estudante.fieb.edu.br>
2025-09-19 08:18:31 -0300

    alteração

--- src/pages/produtos/index.jsx
+++ src/pages/produtos/index.jsx
@@ -1,15 +1,20 @@
 const Produtos = () => {
+    //objeto com a lista de pizzas
+    const pizzas = [
+        'Pizza de Muçarela',
+        'Pizza de calabresa',
+        'Pizza baiana',
+        'Pizza Quatro Queijos'
+    ]
+    //Interação da lista de pizzas
+    const listaPizzas = pizzas.map(pizza => <li>{pizza}</li>)
+
     return (
         <div>
             <ul>
-                <li>Pizza de Muçarela</li>
-                <li>Pizza de Calabreza</li>
-                <li>Pizza portuguesa</li>
-                <li>Pizza baiana</li>
-
+                {listaPizzas}
             </ul>
         </div>
     )
 }
-
 export default Produtos
